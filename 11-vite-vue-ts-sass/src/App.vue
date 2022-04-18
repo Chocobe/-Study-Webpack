3bffc868aebb50935d2e11af7e178fa69a416513
김영우 <kyw05171@gmail.com>
2022-04-19 02:43:26 +0900

    <김영우>: 11-vite-vue-ts-sass 프로젝트를 작성 하였습니다.

---
+++ 11-vite-vue-ts-sass/src/App.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+// import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+
+  <div class="myApp">
+    <h1 class="myApp-title">
+      Hello World
+    </h1>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.myApp {
+  @apply mt-2;
+  @apply py-2 px-5;
+  
+  border: 2px solid #ff1493;
+
+  &-title {
+    @apply font-black;
+    @apply text-pink-400;
+
+    font-size: 30px;
+  }
+}
+</style>
---
+++ 11-vite-vue-ts-sass/src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import "./assets/common.scss";
+
+createApp(App).mount('#app')
